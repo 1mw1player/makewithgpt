bc1f45ea030b2e44bc5d21fbf40a65fd5aa76173
Mustafa Waqar <mustafa@makewithgpt.com>
2023-03-25 00:33:38 +0500

    breaking changes 3

--- experiences/4/index.js
+++ experiences/4/index.js
@@ -24,11 +24,37 @@ let mouseClickPosition = null;
 // Create a counter for destroyed players
 let destroyedPlayers = 0;
 
+function Enemy(x, y, speed) {
+  this.x = x;
+  this.y = y;
+  this.size = player.size;
+  this.color = 'blue';
+  this.speed = speed;
+}
+
+let enemies = [];
+
+
+
+
+
+
 // Function to spawn a new player
 function spawnPlayer() {
-  if (destroyedPlayers >= 10) {
-    player.color = 'blue'; // Change the color of the player if the score is 10 or greater
-    player.speed = 0; // Stop the player from moving if they turn blue
+  if (destroyedPlayers >=5) {
+    player.color = 'green'; // Change the color of the player if the score is 5 or greater
+    player.speed = 2; 
+
+
+    // Spawn new enemies around the player, further away
+    for (let i = 0; i < 5; i++) {
+      let x = player.x + Math.random() * 600 - 150;
+      let y = player.y + Math.random() * 600 - 150;
+      let speed = Math.random() + 1;
+      let enemy = new Enemy(x, y, speed);
+      enemies.push(enemy);
+    }
+  
   } else {
     player.color = 'red'; // Otherwise, set the color to red
     player.speed = 2; // Set the player speed back to 5 if they turn red
@@ -42,6 +68,10 @@ function spawnPlayer() {
     speed: player.speed,
     color: player.color // Set the color of the new player object
   };
+
+  
+
+
 }
 
 let message = '';
@@ -55,6 +85,24 @@ function update() {
       player.angle = Math.atan2(dy, dx);
     }
   }
+  if (player.color === 'green') {
+    // Calculate the angle to the mouse click position
+    if (mouseClickPosition) {
+      const dx = mouseClickPosition.x - player.x;
+      const dy = mouseClickPosition.y - player.y;
+      player.angle = Math.atan2(dy, dx);
+    }
+  }
+
+// Move the enemies towards the player, but at a slower speed
+for (let i = 0; i < enemies.length; i++) {
+  let dx = player.x - enemies[i].x;
+  let dy = player.y - enemies[i].y;
+  let angle = Math.atan2(dy, dx);
+  enemies[i].x += enemies[i].speed * 0.25 * Math.cos(angle);
+  enemies[i].y += enemies[i].speed * 0.25 * Math.sin(angle);
+}
+
 
   // Move the player towards the mouse click position
   if (player.color === 'red') {
@@ -62,11 +110,17 @@ function update() {
     player.y += player.speed * Math.sin(player.angle);
   }
 
+  if (player.color === 'green') {
+    player.x += player.speed * Math.cos(player.angle);
+    player.y += player.speed * Math.sin(player.angle);
+  }
+
+
   // Check if the player has reached the edge of the canvas
   if (player.x < 0 || player.x > canvas4.width || player.y < 0 || player.y > canvas4.height) {
     destroyedPlayers++;
 
-    if (destroyedPlayers % 10 === 0) {
+    if (destroyedPlayers % 5 === 0) {
       spawnPlayer();
       spawnPlayer();
     } else {
@@ -91,6 +145,21 @@ function render() {
   context.fillStyle = player.color;
   context.fillRect(-player.size / 2, -player.size / 2, player.size, player.size);
   context.restore();
+
+// Draw the enemies as triangles
+for (let i = 0; i < enemies.length; i++) {
+  context.save();
+  context.translate(enemies[i].x, enemies[i].y);
+  context.rotate(player.angle);
+  context.fillStyle = enemies[i].color;
+  context.beginPath();
+  context.moveTo(-enemies[i].size / 2, -enemies[i].size / 2);
+  context.lineTo(-enemies[i].size / 2, enemies[i].size / 2);
+  context.lineTo(enemies[i].size / 2, 0);
+  context.closePath();
+  context.fill();
+  context.restore();
+}
 
   // Draw the text at the top of the canvas
   context.fillStyle = 'white';
