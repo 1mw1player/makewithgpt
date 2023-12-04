27e03c7204eebe706eb16c53b80982ece2b63e62
Mustafa Waqar <mustafa@makewithgpt.com>
2023-12-04 21:43:53 +0500

    fail attempt at safari fix

--- assets/clicksound.js
+++ assets/clicksound.js
@@ -1,11 +1,16 @@
-// get a reference to the button element
-const button = document.querySelector('.button');
+// get references to all the button elements with class .button
+const buttons = document.querySelectorAll('.button');
 
 // get a reference to the audio element
-const audio = new Audio('path/to/sound.mp3');
+const audio = new Audio('assets/sounds/fantasy_ui_button_6-102219.mp3');
 
-// add an event listener to the button
-button.addEventListener('click', function() {
-  // play the sound
-  audio.play();
+// add an event listener to each button
+buttons.forEach(function(button) {
+  button.addEventListener('mouseover', function() {
+  
+   // reset the audio to the beginning
+   audio.currentTime = 0;
+   // play the sound
+   audio.play();
+  });
 });
