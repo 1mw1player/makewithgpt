c643d39a3b7414a87e26de2a047ddd547456aa84
Mustafa Waqar <mustafa@makewithgpt.com>
2024-02-26 15:56:48 +0500

    basic structure refactor

--- assets/js/animate.js
+++ assets/js/animate.js
@@ -1,40 +1,17 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const logo = document.querySelector('.logo');
-    const radialEffect = document.querySelector('.radial-blue-effect');
-    let soundEnabled = false; // Flag to track if sound is allowed to play
+document.addEventListener('DOMContentLoaded', function() {
+    var svgElement = document.querySelector('.svg-container svg'); // Ensure this selector matches your SVG container
 
-    // Create an audio object
-    const hoverSound = new Audio('assets/sounds/fantasy_ui_button_6-102219.mp3');
+    function startAnimation() {
+        var cover = svgElement.querySelector('.cover');
+        var text = svgElement.querySelector('.text');
 
-    // Function to play sound
-    function playSound() {
-        if (soundEnabled) {
-            hoverSound.play();
-        }
+        // Apply animations
+        cover.style.animation = 'openBook 2s forwards';
+        text.style.animation = 'fadeInText 2s forwards 2s'; // Delays the text animation to start with the cover opening
+
+        // Remove this event listener to prevent re-triggering the animation
+        svgElement.removeEventListener('click', startAnimation);
     }
 
-    // Enable sound after the first user interaction
-    document.body.addEventListener('click', function () {
-        soundEnabled = true;
-        // Optional: preload the sound here by playing and immediately pausing it
-        hoverSound.play().then(() => {
-            hoverSound.pause();
-            hoverSound.currentTime = 0;
-        }).catch(error => console.log("Sound preloading error:", error));
-    }, { once: true }); // This ensures the event listener is removed after the first execution
-
-    logo.addEventListener('mouseover', function () {
-        playSound();
-        // Apply the radial gradient background on hover
-        radialEffect.style.backgroundImage = 'radial-gradient(circle at center, #164675 0%, #033c63 70%, #003f5c 100%)';
-    });
-
-    logo.addEventListener('mouseout', function () {
-        // Optional: Stop and reset the sound if needed
-        hoverSound.pause();
-        hoverSound.currentTime = 0;
-
-        // Revert to the original background when not hovering
-        radialEffect.style.backgroundImage = 'radial-gradient(circle at center, #ffffff 0%, #ffffff 100%)';
-    });
+    svgElement.addEventListener('click', startAnimation);
 });
