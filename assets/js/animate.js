1964be65117ff85ac4a923de2844a6846fe0fdc2
Mustafa Waqar <mustafa@makewithgpt.com>
2024-02-26 17:58:08 +0500

    story logo

--- assets/js/animate.js
+++ assets/js/animate.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var svgElement = document.querySelector('.svg-container svg'); // Ensure this selector matches your SVG container
+    var svgElement = document.querySelector('.svg-container svg');
 
     function startAnimation() {
-        var cover = svgElement.querySelector('.cover');
+        var bookGroup = svgElement.querySelector('.bookGroup');
         var text = svgElement.querySelector('.text');
 
-        // Apply animations
-        cover.style.animation = 'openBook 2s forwards';
+        // Apply animations to the group, affecting both cover and lines
+        bookGroup.style.animation = 'openBook 2s forwards';
         text.style.animation = 'fadeInText 2s forwards 2s'; // Delays the text animation to start with the cover opening
 
         // Remove this event listener to prevent re-triggering the animation
